91d225b6609d132ce8147e90fd38a69dc1ec8263
Omonte Jeremias <jeremias150817@gmail.com>
2026-03-18 14:15:31 -0300

    feat: add user seeder to create an admin by default

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Jeremias Ezequiel',
+            'email' => 'jere@test.com',
+            'password' => 'abc123',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
